599d28d135d3741cb5d4ccc318bc4d7f80ca892d
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-06-14 17:21:17 +0500

    fout commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let bars = document.querySelector(".nav_bars");
+let navMenu = document.querySelector(".nav_menu");
+let section = document.querySelector("#section");
+bars.addEventListener("click", () => {
+  navMenu.classList.toggle("click_nav_bars");
+  section.classList.toggle("section_mt_5");
+});
