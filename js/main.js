5e0f121b1e80c3823eb9fc0117b31eefa7039c07
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-06-14 09:07:33 +0500

    complate componentss

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let bars = document.querySelector(".nav_bars ");
 let navMenu = document.querySelector(".nav_menu");
+let section = document.querySelector("#section");
 bars.addEventListener("click", () => {
   navMenu.classList.toggle("click_nav_bars");
+  section.classList.toggle("section_mt_5");
   bars.style.display = "none !important";
 });
