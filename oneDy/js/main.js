88755f3031abe6fa999d573f759c8481005fbe50
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-06-28 13:26:36 +0500

    second commit

--- oneDy/js/main.js
+++ oneDy/js/main.js
@@ -1,15 +1,40 @@
-const bars = document.querySelector(".header__bars");
-const times = document.querySelector(".header__times");
-
-const content = document.querySelector(".header__content");
-
-bars.addEventListener("click", () => {
-  content.classList.add("active");
-  bars.style.display = "none";
-  times.style.display = "block";
+new Swiper(".pricing__body", {
+//   loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  clickable: true,
+  simulateTouch: true,
+  touchRatio: 1,
+  touchAngle: 45,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    sensitivity: 4,
+  },
+  slidesPerView: 4,
 });
-times.addEventListener("click", () => {
-  content.classList.remove("active");
-  bars.style.display = "block";
-  times.style.display = "none";
+
+$(function () {
+  $(".header__bars").click(() => {
+    $(".header__content").addClass("active");
+    $(".header__bars").css("display", "none");
+    $(".header__times").css("display", "block");
+  });
+  $(".header__times").click(() => {
+    $(".header__content").removeClass("active");
+    $(".header__times").css("display", "none");
+    $(".header__bars").css("display", "block");
+  });
 });
