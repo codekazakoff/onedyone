bcc1b2a0093ec38aba3e86f9255383a2b61d5cef
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-07-02 17:24:27 +0500

    casc

---
+++ oneDy/js/main.js
@@ -0,0 +1,53 @@
+new Swiper(".pricing__body", {
+  //   loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  clickable: true,
+  simulateTouch: true,
+  touchRatio: 1,
+  touchAngle: 45,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    sensitivity: 4,
+  },
+  slidesPerView: 4,
+});
+
+new Swiper(".blog__body", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  slidesPerView: 2,
+});
+
+$(function () {
+  $(".header__bars").click(() => {
+    $(".header__content").addClass("active");
+    $(".header__bars").css("display", "none");
+    $(".header__times").css("display", "block");
+  });
+  $(".header__times").click(() => {
+    $(".header__content").removeClass("active");
+    $(".header__times").css("display", "none");
+    $(".header__bars").css("display", "block");
+  });
+});
